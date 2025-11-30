dc2c12fe378fc867bf05bb543eeb55936bfd4b80
Toprak <efe.akkilic@ozu.edu.tr>
2025-11-30 18:00:19 +0300

    Adjusted.

--- projects/visualizer/source/app.cpp
+++ projects/visualizer/source/app.cpp
@@ -18,9 +18,9 @@ App::~App() {
 }
 
 std::vector<std::string> getNetIfaces() {
-    static std::filesystem::path network_ifaces_path("/sys/class/net/");
-    if (!std::filesystem::is_directory(network_ifaces_path)) {};
-    std::vector<std::string> out;
+    std::vector<std::string> out(1);
+    std::filesystem::path network_ifaces_path("/sys/class/net/");
+    if (!std::filesystem::is_directory(network_ifaces_path)) return out;
     for (const auto &dir : std::filesystem::directory_iterator{network_ifaces_path}) {
         std::string entry = dir.path().stem();
         std::ifstream type_file{dir.path() / "type", std::ios_base::in};
@@ -62,12 +62,12 @@ void App::run(std::ofstream &log_file) {
         int selected = menu::select(menu);
         switch (selected) {
             case 0:
+                break;
+            case 1:
                 {
                     auto netIfaces = getNetIfaces();
                     menu::display(netIfaces);
                 }
-                break;
-            case 1:
                 break;
             case 2:
                 running = false;
